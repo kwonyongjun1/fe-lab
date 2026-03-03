3fc5e9bf15a9929c34904e499e16a463267029fe
kwonyongjun <dydwns18011@gmail.com>
2026-03-03 10:05:49 +0900

    커밋 테스트

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-
 export default function HomePage() {
+
   return (
     <div className="space-y-3">
       <h1 className="text-2xl font-semibold text-zinc-900 dark:text-zinc-100">
